02e011c5df41c3421ad582c5e4bb97b749b1184f
Cleiver <cleiver@gmail.com>
2019-03-23 16:37:25 -0300

    updating the interface

--- api/src/Proposal/Storage/ProposalStorage.php
+++ api/src/Proposal/Storage/ProposalStorage.php
@@ -8,5 +8,7 @@ interface ProposalStorage {
 
 	public function insertProposal(ProposalEntity $entity): int;
 	public function getProposal(int $proposalId): ProposalEntity;
+	public function updateProposal(ProposalEntity $entity): ProposalEntity;
+	public function getAll(): array;
 
 }
